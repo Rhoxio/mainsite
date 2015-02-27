61337a2599fc6321e7f742cdee3fc5aee4b672ab
Rhoxio <rhoxiodbc@gmail.com>
2015-02-26 20:02:57 -0800

    First push of site content.

---
+++ javascripts/javascript.js
@@ -0,0 +1,20 @@
+ sayingArray = ["Experience is an apt teacher, but wisdom is the medium of creativity.",'To change the world, you need only to speak your mind.', "Existence is the process of learning, and learning is the process of living.", 'Inspiration is a product of passion; a consequence of the human spirit.']
+
+$( document ).ready(function(){
+	$('.tagline').html(sayingArray[Math.floor(Math.random() * sayingArray.length)])
+	
+	var tagFadeIn = function(){
+		$('.tagline').fadeIn(2000, function(){
+			console.log("Fade done. Ready for callback.")
+		})
+	}
+
+	var nameFadeIn = function(){
+		$('.name-desc').fadeIn(1000, function(){
+			tagFadeIn()
+		})
+	}
+
+	nameFadeIn()	
+})
+ 
