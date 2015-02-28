1cc7192392880e5618f2ecdd29ed1e26c3366746
Rhoxio <rhoxiodbc@gmail.com>
2015-02-27 19:51:04 -0800

    More work done on the site. I have a very good idea of how the site is going to look now. I still need to wire everything up, though.

--- javascripts/javascript.js
+++ javascripts/javascript.js
@@ -1,8 +1,14 @@
- sayingArray = ["Experience is an apt teacher, but wisdom is the medium of creativity.",'To change the world, you need only to speak your mind.', "Existence is the process of learning, and learning is the process of living.", 'Inspiration is a product of passion; a consequence of the human spirit.']
+sayingArray = ["Experience is an apt teacher, but wisdom is the medium of creativity.",'To change the world, you need only to speak your mind.', "Existence is the process of learning, and learning is the process of living.", 'Inspiration is a product of passion; a consequence of the human spirit.']
 
 $( document ).ready(function(){
 	$('.tagline').html(sayingArray[Math.floor(Math.random() * sayingArray.length)])
 	
+	$('.about').on('click', function(event){
+		event.preventDefault();
+		var offset = $('.about-me').offset().top - 10;
+		$('html,body').animate({scrollTop: offset}, 1000)
+	})
+
 	var tagFadeIn = function(){
 		$('.tagline').fadeIn(2000, function(){
 			console.log("Fade done. Ready for callback.")
